671dad4b6147c92ee1feb11302ce1e7efee339b5
Natallia Zubareva <Ndobrovolskaya@gmail.com>
2018-07-22 21:35:28 +0300

    Added UICollectionView. Filled with all available fonts.

---
+++ FontViewer/FontCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  FontCollectionViewCell.swift
+//  FontViewer
+//
+//  Created by Natallia Zubareva on 22.07.2018.
+//  Copyright © 2018 Natallia Zubareva. All rights reserved.
+//
+
+import UIKit
+
+class FontCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
